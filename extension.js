51c0fa3bbe404688d25ab47fd7167828733d29c6
Grant Berridge <grantyb@gmail.com>
2010-07-10 23:57:32 +1200

    Calculate the height of the overlay, rather than hard-coding it.  Move the overlay to the top of the page if the cursor moves too close to the bottom of the screen.

--- extension.js
+++ extension.js
@@ -1,11 +1,22 @@
-var overlay = $("<div id='link-overlay'></div>");
+var overlay = $("<div id='link-overlay'>&nbsp;</div>");
 $("body").append($(overlay));
+var overlayHeight = 48; // Initially set to best guess - based on 13px font height, 30px bottom padding and 5px top padding
+var overlayHeightCalculated = false;
 $("a").live("mouseover", function(e) {
-	if ( e.clientY < $(window).height() - 60 ) {
-		$(overlay).text($(this).attr("href")).stop(true,false).animate({"opacity": 0.6},"fast");
-		if ( $(overlay).is(":hidden") ) {
-			$(overlay).show();
-		}
+	if ( !overlayHeightCalculated && $(overlay).outerHeight() != 0 ) {
+		overlayHeight = $(overlay).outerHeight();
+		overlayHeightCalculated = true;
+	}
+	if ( e.clientY < $(window).height() - overlayHeight ) {
+		$(overlay).css("top", "auto");
+		$(overlay).css("bottom", "0");
+	} else {
+		$(overlay).css("top", "0");
+		$(overlay).css("bottom", "auto");
+	}
+	$(overlay).text($(this).attr("href")).stop(true,false).animate({"opacity": 0.6}, "fast");
+	if ( $(overlay).is(":hidden") ) {
+		$(overlay).show();
 	}
 });
 $("a").live("mouseleave", function(e) {
