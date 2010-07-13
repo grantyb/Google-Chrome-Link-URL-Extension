1652c6c464d7cb6943081b45b319540e8c0fb75b
Grant Berridge <grantyb@gmail.com>
2010-07-13 18:45:57 +1200

    Show text of javascript event handlers if the page is jquery-enabled and has click() handlers for the links.

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ var overlay = $("<div id='link-overlay'>&nbsp;</div>");
 $("body").append($(overlay));
 var overlayHeight = 48; // Initially set to best guess - based on 13px font height, 30px bottom padding and 5px top padding
 var overlayHeightCalculated = false;
-$("a").live("mouseover", function(e) {
+$("a[href],area[href]").live("mouseover", function(e) {
 	if ( !overlayHeightCalculated && $(overlay).outerHeight() != 0 ) {
 		overlayHeight = $(overlay).outerHeight();
 		overlayHeightCalculated = true;
@@ -14,12 +14,46 @@ $("a").live("mouseover", function(e) {
 		$(overlay).css("top", "0");
 		$(overlay).css("bottom", "auto");
 	}
-	$(overlay).text($(this).attr("href")).stop(true,false).animate({"opacity": 0.6}, "fast");
+	var handlerText = $(this).attr("href");
+	$(overlay).text(handlerText).stop(true,false).animate({"opacity": 0.6}, "fast");
 	if ( $(overlay).is(":hidden") ) {
 		$(overlay).show();
 	}
+	$(".chrome-link-viewer-extension").removeClass("chrome-link-viewer-extension");
+	$(this).addClass("chrome-link-viewer-extension");
+	var script = $("<script type='text/javascript' charset='utf-8'>\
+		if ( typeof(jQuery) !== 'undefined' ) {\
+			var events = $('.chrome-link-viewer-extension').data('events');\
+			if ( events ) {\
+				var clicks = events.click;\
+				if ( clicks ) {\
+					var eventSource = '<ul>';\
+					jQuery.each(clicks, function(name, value) {\
+						var txt = '' + value.handler;\
+						txt = txt.replace(/&/g,'&amp;');\
+						txt = txt.replace(/</g,'&lt;');\
+						txt = txt.replace(/>/g,'&gt;');\
+						txt = txt.replace(/\\n/g,'<br/>');\
+						txt = txt.replace(/\\t/g,'&nbsp;&nbsp;');\
+						txt = txt.replace(/ /g,'&nbsp;');\
+						eventSource = eventSource + '<li>' + txt + '</li>';\
+					});\
+					eventSource = eventSource + '</ul>';\
+					$('.chrome-link-viewer-extension').attr('data-events', eventSource);\
+				}\
+			}\
+		}\
+	</script>");
+	$("body").append($(script));
+	setTimeout(function(){
+		var events = $('.chrome-link-viewer-extension').attr("data-events");
+		$(".chrome-link-viewer-extension").removeClass("chrome-link-viewer-extension");
+		if ( events ) {
+			$(overlay).append($(events));
+		}
+	}, 10);
 });
-$("a").live("mouseleave", function(e) {
+$("a[href],area[href]").live("mouseleave", function(e) {
 	$(overlay).animate({"opacity": 0.0});
 });
 $(overlay).mouseenter(function(e){
